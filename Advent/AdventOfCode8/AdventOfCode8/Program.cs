40ad15bcc72ae64fbdaefddc9bf8946359bc04a1
Vladimir Bobov <vbobov@microsoft.com>
2016-12-08 14:40:58 -0800

    Day 8 initial commit

---
+++ Advent/AdventOfCode8/AdventOfCode8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode8
+{
+    class Program
+    {
+        static readonly int Height = 6;
+        static readonly int Width = 50;
+        static bool[,] Grid = new bool[Height,Width];
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
