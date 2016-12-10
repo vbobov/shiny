8893ee5aa721733ec29b4db29ac6609854b218ea
Vladimir Bobov <vbobov@microsoft.com>
2016-12-09 16:03:11 -0800

    Day 9 initial

---
+++ Advent/AdventOfCode9/AdventOfCode9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+
+            }
+        }
+    }
+}
